bc94062beb6ca165f5edcd0f6f1256a1c0ad102b
Nooc <nooc@nooc.me>
2024-02-18 23:03:55 +0800

    Update middleware config matcher

--- middleware.ts
+++ middleware.ts
@@ -42,10 +42,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    // Skip all internal paths (_next)
-    "/((?!_next).*)",
-    // Optional: only run on root (/) URL
-    // '/'
-  ],
+  matcher: ["/", "/works"],
 };
